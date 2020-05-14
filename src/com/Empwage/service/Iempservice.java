c7025e7ea08de052360a0d85039cf63d63732e05
seetharamulu <thumurugoti.seetharamulu@gmail.com>
2020-05-14 20:08:11 +0530

    added employee wage

--- src/com/Empwage/service/Iempservice.java
+++ src/com/Empwage/service/Iempservice.java
@@ -4,6 +4,6 @@ import com.Empwage.model.Employee;
 public interface Iempservice {
 
 	public void EmpStatus(Employee emp);
-	public void EmpDailywage(Employee emp);
-	public void EmpSalary(Employee emp);
+	public void EmpDailywage(Employee emp, int hour_wage);
+	public void EmpSalary(Employee emp, String name , int hour_wage, int monthly_days, int monthly_hours);
 }
