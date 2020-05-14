e799b84bcf65b3683ac37d8f10791b047b5f882d
seetharamulu <thumurugoti.seetharamulu@gmail.com>
2020-05-14 13:01:47 +0530

    added employee wage

---
+++ src/com/Empwage/model/Employee.java
@@ -0,0 +1,13 @@
+package com.Empwage.model;
+public class Employee{
+
+
+        private boolean status;
+
+        public boolean getStatus(){
+                return status;
+        }
+        public void setStatus(boolean status){
+                this.status=status;
+        }
+}
