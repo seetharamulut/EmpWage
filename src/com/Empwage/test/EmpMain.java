bffa40c3256ab76c7ab37edb0eb4add28f32a938
seetharamulu <thumurugoti.seetharamulu@gmail.com>
2020-05-15 20:55:19 +0530

    added employee wage

--- src/com/Empwage/test/EmpMain.java
+++ src/com/Empwage/test/EmpMain.java
@@ -1,13 +1,14 @@
 package com.Empwage.test;
 import com.Empwage.model.Employee;
 import com.Empwage.service.EmpwageBuilderarray;
+import com.Empwage.service.Iwagecomputation;
 
 public class EmpMain{
 
         public static void main(String[] args) {
 		System.out.println("welcome to employee wage computation");
 
-		EmpwageBuilderarray empwagebuilderarray = new EmpwageBuilderarray();
+		Iwagecomputation empwagebuilderarray = new EmpwageBuilderarray();
 		empwagebuilderarray.addempwage("Dmart", 20, 21, 100);
 		empwagebuilderarray.addempwage("Reliance", 18, 24, 120);
 		empwagebuilderarray.addempwage("Tata", 22, 25, 125);
